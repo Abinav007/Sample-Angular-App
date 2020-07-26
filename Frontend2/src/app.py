d8215db925381cc2a14c50b1ba84386f222a5d27
shyam2407 <shyamv.barath9@gmail.com>
2020-07-26 08:29:09 +0530

    Update app.py

--- Frontend2/src/app.py
+++ Frontend2/src/app.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 22:48:54 2020
+
+@author: uib02367
+"""
 import random, json
 import requests
 from flask import Flask, render_template, request
@@ -21,21 +27,39 @@ def about_1():
     return "<h1> Hello <h1>"
 """
 
-@app.route('/form_name', methods=['POST','GET'] )
+@app.route('/', methods=['POST','GET'] )
 def form_Name():
     if request.method=='POST':
         firstName=request.form["firstname"]
-        r=requests.post('http://httpbin.org/post?'+"firstname="+firstName, json={'Found':random.choice([True, False]), 'LastName':'Doe'})
-        bool_1=json.loads(r.json()['data'])['Found']
-       
-        if bool_1:
-            lastName=json.loads(r.json()['data'])['LastName']
-            return '''
-                   <p>Name found!</p>
-                   <p>Welcome {} {} </p>
-                   '''.format(firstName,lastName)
+        BACKEND_URL = "http://backend-service/users"
+        PARAMS={"firstName":firstName}
+        
+        r=requests.get(url=BACKEND_URL,params=PARAMS)
+        #bool_1=json.loads(r.json()['data'])['Found']
+        #return(r.encode())
+        #return str(r.status_code)
+        if r.status_code==200:
+            fName=r.json()['firstName']
+            if fName.lower()==firstName.lower():
+                lastName=r.json()['lastName']
+                return '''
+                       <p>Name found!</p>
+                       <p>Welcome {} {} </p>
+                       '''.format(firstName,lastName)
+            else:
+                return '''
+                       <p>Wrong name fetched from the database</p>
+                       <p>Firstname requested: {} </p>
+                       <p>Firstname from database: {} </p>
+                       '''.format(firstName,fName)
+        elif r.status_code==201:
+            return '<p>Name not found in the database :(</p>'
+        elif r.status_code==500:
+            return '<p>ERROR: Check the backend!</p>'
         else:
-            return '<p>Name not found :(</p>'
+            return '<p>ERROR: Check your code!</p>'
+        #except Exception as e:
+        #print(e)
     return '''<form method="POST">
               <h3> Please enter the firstname to be fetched</h3>
               Firstname <input type="text" name="firstname">
@@ -44,4 +68,4 @@ def form_Name():
               '''
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True, port=5000)
